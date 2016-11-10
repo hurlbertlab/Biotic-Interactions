b77ae6adf5cc1a6f769e5f46ddccb324018b6d3d
Snell <ssnell@ad.unc.edu>
2016-11-10 12:51:02 -0500

    working on bbs_pool/main comp abun

--- Biotic_Interactions_Snell_Data_cleaning.R
+++ Biotic_Interactions_Snell_Data_cleaning.R
@@ -135,7 +135,7 @@ write.csv(new_spec_weights, "new_spec_weights.csv", row.names=FALSE)
 routes = unique(temp_occ$stateroute)
 # merge expected presence data with species name information
 expect_pres$FocalAOU[expect_pres$FocalAOU == 7220] <- 7222
-sub_ep = merge(expect_pres[,c('stateroute', 'FocalAOU')], focal_AOU, by.x = 'FocalAOU',by.y="focalAOU", all.x=TRUE) # want all = TRUE for exp pres
+sub_ep = merge(expect_pres[,c('stateroute', 'FocalAOU')], focal_AOU, by.x = 'FocalAOU', by.y="focalAOU", all.x=TRUE) # want all = TRUE for exp pres
 # merge expected presence with occupancy data
 new_occ = merge(sub_ep, temp_occ, by.x = c('stateroute', 'FocalAOU'), by.y = c('stateroute', 'Aou'), all=TRUE) 
 new_occ$n[is.na(new_occ$n)] <- 0
@@ -148,7 +148,7 @@ new_occ2 = new_occ[new_occ$stateroute %in% routes, ]
 focalspecies = unique(new_spec_weights$focalAOU)
 # need to change winter wren AOU to 7222 from 7220 in bbs_pool
 bbs$Aou[bbs$Aou == 7220] <- 7222
-# filter BBS mean abundance by AOU/stateroute by year --------1996-2010 FIX
+# filter BBS mean abundance by AOU/stateroute by year -------- 
 bbs_pool = bbs %>% 
   group_by(stateroute, Aou) %>% 
   dplyr::summarize(abundance = mean(SpeciesTotal)) %>%
@@ -156,7 +156,7 @@ bbs_pool = bbs %>%
 names(bbs_pool)[names(bbs_pool)=="Aou"] <- "AOU"
 
 # merge in occupancies of focal
-occ_abun = merge(bbs_pool, new_occ2, by.x = c("AOU", "stateroute"),by.y = c("FocalAOU", "stateroute"))
+occ_abun = merge(bbs_pool, new_occ, by.x = c("AOU", "stateroute"),by.y = c("FocalAOU", "stateroute"), all=TRUE)
 names(occ_abun)[names(occ_abun)=="abundance"] <- "FocalAbun"
 
 # Take range overlap area to assign "main competitor" for each focal species
