8e9689638283c792a876fc521cf5e1ee1ac6a7f2
Snell <ssnell@ad.unc.edu>
2017-02-15 14:52:20 -0500

    toying with gimms database

--- BI abiotic script.R
+++ BI abiotic script.R
@@ -56,6 +56,130 @@ circs.sp = SpatialPolygons(circs, proj4string=CRS(prj.string))
 plot(circs.sp)
 
 #####################################################################################
+#### ----NDVI ----#####
+start_yr <- 2001
+end_yr <- 2015
+min_num_yrs <- 15
+richness_w_env <- get_richness_ts_env_data(start_yr, end_yr, min_num_yrs)
+
+#################################################
+#This assumes we want all gimms files that are available. It queries for files
+#that are available for download and compares against files already in the gimms
+#data directory.
+#Returns a list of files to download, which may be length 0.
+##################################################
+get_gimms_download_list=function(gimms_folder = './data/gimms_ndvi/'){
+  available_files_download_path=gimms::updateInventory()
+  available_files_name=basename(available_files_download_path)
+  
+  files_present=list.files(gimms_folder)
+  #hdr files are created from some of the gimms processing that we don't want to
+  #use here.
+  files_present=files_present[!grepl('hdr', files_present)]
+  
+  to_download=available_files_download_path[! available_files_name %in% files_present]
+  
+  return(to_download)
+}
+
+################################################
+#Extract values from a single gimms file given a set of coordinates
+################################################
+#' @importFrom gimms rasterizeGimms
+gimms_file_path = "Z:/GIS/MODIS NDVI/2000_2016/GIMMS"
+route_locations = routes.laea
+extract_gimms_data=function(gimms_file_path, route_locations){
+  #Have to load and extract twice. Once for the actual NDVI, once for the quality flag.
+  gimmsRaster=gimms::rasterizeGimms(gimms_file_path, flag=FALSE)
+  ndvi=raster::extract(gimmsRaster, route_locations)
+  gimmsRaster=gimms::rasterizeGimms(gimms_file_path, flag=TRUE)
+  flag=raster::extract(gimmsRaster, route_locations)
+  
+  year=as.numeric(substr(basename(gimms_file_path), 4,5))
+  month=substr(basename(gimms_file_path), 6,8)
+  day=substr(basename(gimms_file_path), 11,11)
+  
+  #Convert the a b to the 1st and 15th
+  day=ifelse(day=='a',1,15)
+  
+  #Convert 2 digit year to 4 digit year
+  year=ifelse(year>50, year+1900, year+2000)
+  
+  return(data.frame(year=year, month=month, day=day, ndvi=ndvi, flag=flag, site_id=route_locations@data$site_id, stringsAsFactors = FALSE))
+}
+
+################################################
+#Extract the NDVI time series for all bbs routes
+#from all years of gimms data
+################################################
+
+#' @importFrom dplyr bind_rows
+#' @importFrom dplyr %>%
+#' @importFrom sp coordinates<-
+process_gimms_ndvi_bbs=function(gimms_folder = './data/gimms_ndvi/'){
+  
+  bbs_data <- get_bbs_data()
+  route_locations <- unique(dplyr::select(bbs_data, site_id, long, lat))
+  coordinates(route_locations) <- c("long", "lat")
+  
+  gimms_files=list.files(gimms_folder, full.names = TRUE)
+  #hdr files are created from some of the gimms processing that we don't want to
+  #use here.
+  gimms_files=gimms_files[!grepl('hdr', gimms_files)]
+  
+  gimms_ndvi_bbs=data.frame()
+  for(file_path in gimms_files){
+    gimms_ndvi_bbs=extract_gimms_data(file_path, route_locations) %>%
+      bind_rows(gimms_ndvi_bbs)
+  }
+  save_provenance(gimms_ndvi_bbs)
+  return(gimms_ndvi_bbs)
+}
+
+
+src_sqlite <- function(path, create = FALSE) {
+  if (!requireNamespace("RSQLite", quietly = TRUE)) {
+    stop("RSQLite package required to connect to sqlite db", call. = FALSE)
+  }
+  
+  if (!create && !file.exists(path)) {
+    stop("Path does not exist and create = FALSE", call. = FALSE)
+  }
+  
+  con <- DBI::dbConnect(RSQLite::SQLite(), path)
+  RSQLite::initExtension(con)
+  
+  src_sql("sqlite", con, path = path)
+}
+
+get_bbs_gimms_ndvi = function(
+  sqlite_db_file='./data/bbsforecasting.sqlite',
+  database = src_sqlite(sqlite_db_file, create = TRUE),
+  gimms_folder = './data/gimms_ndvi/'
+){
+  dir.create(gimms_folder, showWarnings = FALSE, recursive = TRUE)
+  
+  if('gimms_ndvi_bbs_data' %in% dplyr::src_tbls(database)){
+    return(collect(tbl(database, sql('SELECT * from gimms_ndvi_bbs_data')), n = Inf))
+  } else {
+    print('Gimms NDVI bbs data not found, processing from scratch')
+    
+    files_to_download=get_gimms_download_list()
+    if(length(files_to_download)>0){
+      print('Downloading GIMMS data')
+      downloadGimms(x=files_to_download, dsn=gimms_folder)
+    }
+    
+    gimms_ndvi_bbs_data=process_gimms_ndvi_bbs()
+    
+    gimms_ndvi_bbs_data=filter_gimms_data(gimms_ndvi_bbs_data)
+    
+    copy_to(database, gimms_ndvi_bbs_data, temporary = FALSE,
+            indexes = list(c('site_id','year','month')))
+    
+    return(gimms_ndvi_bbs_data)
+  }
+}
 #### ----EVI ----#####
 setwd('Z:/GIS/EVI')
 
