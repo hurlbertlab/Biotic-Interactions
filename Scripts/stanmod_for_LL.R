60c293f3b2fa25c05e4f7bb33e3d19c627f31167
ssnell6 <ssnell@ad.unc.edu>
2018-12-20 10:51:53 -0500

    backup for IBS

--- Scripts/stanmod_for_LL.R
+++ Scripts/stanmod_for_LL.R
@@ -1,19 +1,10 @@
 #r mod for LL
 library(rstanarm)
 
-occuenv = read.csv("all_expected_pres.csv", header = TRUE) 
-occumatrix$c_s = scale(occumatrix$comp_scaled, scale = T, center = T)
-occumatrix$abTemp=abs(occumatrix$zTemp)
-occumatrix$abElev=abs(occumatrix$zElev)
-occumatrix$abPrecip=abs(occumatrix$zPrecip)
-occumatrix$abNDVI=abs(occumatrix$zNDVI)
+occumatrix = read.csv("occumatrix.csv", header = TRUE) 
 
-# using equation species sum*Focal occ to get success and failure for binomial anlaysis
-occumatrix$sp_success = 15 * occumatrix$FocalOcc
-occumatrix$sp_fail = 15 * (1 - occumatrix$FocalOcc)
 
-occumatrix$sp_success = as.integer(occumatrix$sp_success)
-occumatrix$sp_fail = as.integer(occumatrix$sp_fail)
-mm <- stan_glmer(cbind(sp_success, sp_fail) ~ c_s + 
-       abTemp + abElev + abPrecip + abNDVI + (c_s + abTemp + abElev + abPrecip + abNDVI|FocalAOU), family = binomial(link = logit), data = occumatrix, iter = 10000, prior_covariance = decov(regularization = 1, concentration = 1, shape = 1, scale = 1))
-write.csv(summary(mm), row.names= TRUE)
+mmslope <- stan_glmer(cbind(sp_success, sp_fail) ~ c_s + 
+abTemp + abElev + abPrecip + abNDVI + (c_s + abTemp + abElev + abPrecip + abNDVI|FocalAOU), family = binomial(link = logit), data = occumatrix,warmup=5000,iter=10000, chains = 4, cores = 4, prior_covariance = decov(regularization = 1, concentration = 1, shape = 1, scale = 1))
+save(mmslope, filename ="mmslope.rda")
+write.csv(mmslope, "mmslope.csv", row.names = FALSE)
