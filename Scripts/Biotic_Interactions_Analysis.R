d92a9dd5d027a5cb7e22f8466ee91aba8028c4cf
ssnell6 <ssnell@ad.unc.edu>
2019-01-30 15:21:29 -0500

    mm slope for all comps 500 iter

--- Scripts/Biotic_Interactions_Analysis.R
+++ Scripts/Biotic_Interactions_Analysis.R
@@ -189,9 +189,10 @@ occumatrix$sp_fail = as.integer(15 * (1 - occumatrix$FocalOcc))
 library(brms)
 library(rstudioapi)
 mmslope <- brm(sp_success | trials(sp_success+sp_fail) ~ c_s +  abTemp + abElev + abPrecip + abNDVI + (c_s + abTemp + abElev + abPrecip + abNDVI|FocalAOU), family = binomial(link = logit), data = occumatrix , cores = 2, chains=4, iter=500,warmup=200,control = list(max_treedepth = 15),set_prior("lkj(1)", class = "cor"))
+mm_summ = data.frame(mmslope)
 
-# write.csv(summary(mm), "mm_slope.csv", row.names= TRUE)
-
+# write.csv(mm_summ, "data/mm_slope_brms.csv", row.names= FALSE)
+mod2 <- readRDS("mmslope_brms")
 # mmint <- stan_glmer(cbind(sp_success, sp_fail) ~ c_s + abTemp + abElev + abPrecip + abNDVI + (1|FocalAOU), family = binomial(link = logit), data = occumatrix, iter = 10000, prior_covariance = decov(regularization = 1, concentration = 1, shape = 1, scale = 1))
 
 # Default priors =  default_prior_test <- stan_glmer(cbind(sp_success, sp_fail) ~ c_s + 
